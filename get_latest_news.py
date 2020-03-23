64b28351dff5c28a0754b2f2854bc4420881bd08
Diven Kumar Sambhwani <diven.kumar.sambhwani@smu.ca>
2020-03-22 23:45:50 -0400

    script to get latest news

---
+++ get_latest_news.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup as bs
+import requests 
+import json
+
+
+def get_latest_news():
+
+  #website to get latest news
+  newsroom_ontario = 'https://news.ontario.ca/archive/en'
+  urls = [newsroom_ontario]
+  #Format used in website
+  formats = ['html.parser']
+  #Tags used for heading
+  tags = ['h3']
+
+  news_dict=[]
+  for url in urls:
+    response=requests.get(url)
+    soup = bs(response.content, formats[0])
+
+    for link in soup.find_all(tags[0]):
+      if(len(link.text.split(" ")) > 3):
+        news_dict.append({'url':url, 'headline':link.text})
+      
+
+    news_dict.pop()
+    return news
+
+
+if __name__ == '__main__':
+  news = get_latest_news()
+  with open('data/processed/latest_news.json', 'w') as json_file:
+    json_file.write(json.dumps(json.loads(news), indent=2))
+
+
+
+
+
+
+
+
+    
