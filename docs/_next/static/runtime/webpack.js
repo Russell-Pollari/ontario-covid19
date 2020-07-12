d209adde4051968f19b7ecfaa59a39cb6ad65842
russell-pollari <pollarir@mgail.com>
2020-07-12 10:10:33 -0400

    Build static site

--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c37423c933f566c0acb8";
+/******/ 	var hotCurrentHash = "3744a105bad6a64f74a8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
