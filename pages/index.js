54384607e41753fcc1a9ec1083dcba7197223639
russell-pollari <pollarir@mgail.com>
2020-07-12 09:58:58 -0400

    Move metabse urls to constants.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Layout from '../components/Layout';
 import MetabaseIFrame from '../components/MetabaseIFrame';
 
+import { ontarioMetabseUrl } from '../constants';
+
 function HomePage() {
 	return (
 		<Layout>
-			<MetabaseIFrame
-				url="https://ontario-covid-dash-metabaase.herokuapp.com/public/dashboard/10bf46a5-a37f-4686-9067-ab9f312c0767"
-			/>
+			<MetabaseIFrame url={ontarioMetabseUrl} />
 		</Layout>
 	);
 }
--- pages/logs.js
+++ pages/logs.js
@@ -1,14 +1,14 @@
 import Layout from '../components/Layout';
 import MetabaseIFrame from '../components/MetabaseIFrame';
 
+import { logsMetabaseUrl } from '../constants';
+
 export default function Logs() {
 	return (
 		<Layout
 			title="Covid-19 in Ontario | Logs"
 			currentPath="/logs">
-			<MetabaseIFrame
-				url="https://ontario-covid-dash-metabaase.herokuapp.com/public/question/63aa2160-87fc-482b-80b6-bf76a6d058ed"
-			/>
+			<MetabaseIFrame url={logsMetabaseUrl} />
 		</Layout>
 	);
 };
