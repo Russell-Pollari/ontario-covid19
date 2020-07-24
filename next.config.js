1f85173b75769b99ac15c580af1b640008e836ed
russell-pollari <pollarir@mgail.com>
2020-07-23 21:00:08 -0400

    Remove config

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const basePath = '/ontario-covid19'
-const isDev = process.env.NODE_ENV === 'development'
-
-module.exports = {
-	assetPrefix: isDev ? '' : basePath,
-	publicRuntimeConfig: {
-		basePath: isDev ? '' : basePath,
-	}
-}
