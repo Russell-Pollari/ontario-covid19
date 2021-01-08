31f2ed3681019e6f72843a4437c4944c32339e2b
russell-pollari <pollarir@mgail.com>
2021-01-07 21:01:36 -0500

    Fix About page layout

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,8 @@
+import App from '../components/App';
 import Layout from '../components/Layout';
 
 import { resources, contactDetails } from '../constants.js';
+
 
 const ContactDetail = ({ title, label, href }) => (
 	<li>
@@ -10,6 +12,7 @@ const ContactDetail = ({ title, label, href }) => (
 		</a>
 	</li>
 );
+
 
 const Resource = ({ label, href, description }) => (
 	<div className="mv16">
@@ -30,38 +33,34 @@ const Resource = ({ label, href, description }) => (
 
 export default function About() {
 	return (
-		<Layout
-			title="Covid-19 in Ontario | About"
-			currentPath="/about">
-			<div className="tc pa32">
-				<div className="dib tl w-100 maw768">
-					<p>
-						Hi there,
-					</p>
-					<p>
-						To solve my own curiosity, I put together this dashboard to track
-						Ontario's Covid-19 numbers. Many other folks have found it useful,
-						and I've done my best to keep it accurate and up to date, as well as
-						add new plots and data that y'all have suggested.
-					</p>
-					<p>
-						Feel free to get in touch:
-					</p>
-					<ul>
-						{contactDetails.map((detail) => (
-							<ContactDetail key={detail.title} {...detail} />
-						))}
-					</ul>
-					<div className="mv16">
-						<h2>
-							Data and resources
-						</h2>
-						{resources.map((resource) => (
-							<Resource key={resource.label} {...resource} />
-						))}
-					</div>
+		<div className="tc pa32">
+			<div className="dib tl w-100 maw768">
+				<p>
+					Hi there,
+				</p>
+				<p>
+					To solve my own curiosity, I put together this dashboard to track
+					Ontario's Covid-19 numbers. Many other folks have found it useful,
+					and I've done my best to keep it accurate and up to date, as well as
+					add new plots and data that y'all have suggested.
+				</p>
+				<p>
+					Feel free to get in touch:
+				</p>
+				<ul>
+					{contactDetails.map((detail) => (
+						<ContactDetail key={detail.title} {...detail} />
+					))}
+				</ul>
+				<div className="mv16">
+					<h2>
+						Data and resources
+					</h2>
+					{resources.map((resource) => (
+						<Resource key={resource.label} {...resource} />
+					))}
 				</div>
 			</div>
-		</Layout>
-	)
-}
+		</div>
+	);
+};
