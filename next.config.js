f3c4d57b484cf2295b094374d974fcdc2440ff95
russell-pollari <pollarir@mgail.com>
2021-01-28 08:56:26 -0500

    Fix about page link and build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-const basePath = '/ontario-covid19'
 const isDev = process.env.NODE_ENV === 'development'
+const basePath = isDev ? '/ontario-covid19' : '';
 
 module.exports = {
-	assetPrefix: isDev ? '' : basePath,
+	assetPrefix: basePath,
 	publicRuntimeConfig: {
-		basePath: isDev ? '' : basePath,
+		basePath: basePath,
 	}
 }
--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,11 @@ function HomePage() {
 				Want to contribute to this dashboard? Fork it on <a href="https://github.com/Russell-Pollari/ontario-covid19">GitHub</a>.
 			</p>
 			<p>
-				<a href="/about">About this dashboard</a>
+				<Link href="/about">
+					<a>
+						About this dashboard
+					</a>
+				</Link>
 			</p>
 			<OntarioStatusTable dataSource={data} />
 			<ChartContainer
