bf72122c05f5d3d21ee823e57e4bc90261941d7d
russell-pollari <pollarir@mgail.com>
2021-01-28 09:04:38 -0500

    Fixup

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const isDev = process.env.NODE_ENV === 'development'
-const basePath = isDev ? '/ontario-covid19' : '';
+const basePath = isDev ? '' : '/ontario-covid19';
 
 module.exports = {
 	assetPrefix: basePath,
