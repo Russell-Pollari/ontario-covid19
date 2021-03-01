d825412a6a5823287adcd8b827e25cfc2c37d155
IsaacBerman <isaac@interaptix.com>
2021-03-01 17:07:24 -0500

    Add hospitalizations 7-day average

--- components/charts/ontario/hospitalized.js
+++ components/charts/ontario/hospitalized.js
@@ -1,11 +1,22 @@
 const hospitalized = {
-	title: 'Patients hospitalized',
-	dataKeyX: 'date_string',
-	bars: [{
-		dataKey: 'Number of patients hospitalized with COVID-19',
-		name: 'Patients hospitalized',
-		fill: '#ef8c8c',
-	}],
+  title: "Patients hospitalized",
+  dataKeyX: "date_string",
+  bars: [
+    {
+      dataKey: "Number of patients hospitalized with COVID-19",
+      name: "Patients hospitalized",
+      fill: "#ef8c8c",
+    },
+  ],
+  lines: [
+    {
+      dataKey: "hospitalizations_rolling_average",
+      name: "7 day rolling average",
+      stroke: "black",
+      strokeWidth: 2,
+      dot: false,
+    },
+  ],
 };
 
 export default hospitalized;
