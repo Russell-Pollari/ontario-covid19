44d5ee81a65e25dff5d61a0c0c0957f5df0d42b3
IsaacBerman <isaac@interaptix.com>
2021-03-02 14:28:28 -0500

    Revert "Add hospitalizations 7-day average"
    This reverts commit d825412a6a5823287adcd8b827e25cfc2c37d155.

--- components/charts/ontario/hospitalized.js
+++ components/charts/ontario/hospitalized.js
@@ -1,22 +1,11 @@
 const hospitalized = {
-  title: "Patients hospitalized",
-  dataKeyX: "date_string",
-  bars: [
-    {
-      dataKey: "Number of patients hospitalized with COVID-19",
-      name: "Patients hospitalized",
-      fill: "#ef8c8c",
-    },
-  ],
-  lines: [
-    {
-      dataKey: "hospitalizations_rolling_average",
-      name: "7 day rolling average",
-      stroke: "black",
-      strokeWidth: 2,
-      dot: false,
-    },
-  ],
+	title: 'Patients hospitalized',
+	dataKeyX: 'date_string',
+	bars: [{
+		dataKey: 'Number of patients hospitalized with COVID-19',
+		name: 'Patients hospitalized',
+		fill: '#ef8c8c',
+	}],
 };
 
 export default hospitalized;
