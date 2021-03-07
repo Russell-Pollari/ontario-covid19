39fdc40afc367940532e6ec704d45b0a0cbea7be
isaac <isaac@interaptix.com>
2021-03-07 13:06:43 -0500

    Add vaccine 7 day average (#68)
    * Add hospitalizations 7-day average
    
    * Revert "Add hospitalizations 7-day average"
    
    This reverts commit d825412a6a5823287adcd8b827e25cfc2c37d155.
    
    * Add vaccine 7-day average

--- components/charts/vaccinations/dailyDoses.js
+++ components/charts/vaccinations/dailyDoses.js
@@ -1,11 +1,22 @@
 const dailyDoses = {
-	dataKeyX: "date_string",
-	title: "Daily vaccines administered",
-	bars: [{
-		dataKey: 'previous_day_doses_administered',
-		name: 'Daily doses administered',
-		fill: '#509ee3',
-	}]
+  dataKeyX: "date_string",
+  title: "Daily vaccines administered",
+  bars: [
+    {
+      dataKey: "previous_day_doses_administered",
+      name: "Daily doses administered",
+      fill: "#509ee3",
+    },
+  ],
+  lines: [
+    {
+      dataKey: "new_vaccines_rolling_average",
+      name: "7 day rolling average",
+      stroke: "black",
+      strokeWidth: 2,
+      dot: false,
+    },
+  ],
 };
 
 export default dailyDoses;
