df44d3c7619ba533245931afd85c445debd47fca
Russell Pollari <pollarir@gmail.com>
2021-03-14 13:25:24 -0400

    Prettify OntarioStatusTable (#82)
    Co-authored-by: russell-pollari <pollarir@mgail.com>

--- components/OntarioStatusTable.js
+++ components/OntarioStatusTable.js
@@ -1,96 +1,67 @@
-import { useState, useEffect, useCallback } from 'react';
-
 const hospitalField = 'Number of patients hospitalized with COVID-19';
 const icuField = 'Number of patients in ICU with COVID-19';
 
+import DataTable from './DataTable';
+
+const formatNumber = val => val.toLocaleString();
+
+const formatDelta = val => (
+	`${val >= 0 ? '+' : ''}${formatNumber(val)}`
+);
+
 const OntarioStatusTable = ({ dataSource = [] }) => {
-	const [data, setData] = useState([]);
-	const [entryAmount, setEntryAmount] = useState(4);
-
-	useEffect(() => {
-		const data = dataSource.slice(-entryAmount).reverse();
-		setData(data);
-	}, [dataSource, entryAmount]);
-
-	const showMore = useCallback(() => {
-		setEntryAmount((prev) => prev + 4);
-	}, [setEntryAmount]);
-
-	const showLess = useCallback(() => {
-		setEntryAmount((prev) => (prev > 4 ? prev - 4 : prev));
-	}, [setEntryAmount]);
+	const columns = [{
+		label: 'Date',
+		key: 'date_string',
+	}, {
+		label: 'Active cases',
+		key: 'active_cases',
+		formatValue: formatNumber,
+	}, {
+		label: '',
+		key: 'new_active_cases',
+		formatValue: formatDelta,
+		highlight: true,
+	}, {
+		label: 'Total cases',
+		key: 'Total Cases',
+		formatValue: formatNumber,
+	}, {
+		label: '',
+		key: 'new_cases',
+		formatValue: formatDelta,
+		highlight: true,
+	}, {
+		label: 'Deaths',
+		key: 'Deaths',
+		formatValue: formatNumber,
+	}, {
+		label: '',
+		key: 'new_deaths',
+		formatValue: formatDelta,
+		highlight: true,
+	}, {
+		label: 'Patients hospitalized',
+		key: hospitalField,
+		formatValue: formatNumber,
+	}, {
+		label: '',
+		key: 'new_hospital',
+		formatValue: formatDelta,
+		highlight: true,
+	}, {
+		label: 'Patients in ICU',
+		key: icuField,
+		formatValue: formatNumber,
+	}, {
+		label: '',
+		key: 'new_icu',
+		formatValue: formatDelta,
+		highlight: true,
+	}];
 
 	return (
-		<div className="tl chart-container">
-			<table className="w-100">
-				<thead>
-					<tr>
-						<th>Date</th>
-						<th colSpan="2">
-							<a href="#Active cases">Active cases</a>
-						</th>
-						<th colSpan="2">
-							<a href="#Total cases">Cases</a>
-						</th>
-						<th colSpan="2">
-							<a href="#Total deaths">Deaths</a>
-						</th>
-						<th colSpan="2">
-							<a href="#Patients hospitalized">Hospitalizations</a>
-						</th>
-						<th colSpan="2">
-							<a href="#Patients in ICU">Patients in ICU</a>
-						</th>
-						<th colSpan="2">Resolved</th>
-					</tr>
-				</thead>
-				<tbody>
-					{data.map((status, index) => (
-						<tr key={index}>
-							<td>{status.date_string}</td>
-							<td>{status.active_cases.toLocaleString()}</td>
-							<td>
-								({status.new_active_cases >= 0 && '+'}
-								{status.new_active_cases.toLocaleString()})
-							</td>
-							<td>{status['Total Cases'].toLocaleString()}</td>
-							<td>(+{status.new_cases.toLocaleString()})</td>
-							<td>{status['Deaths'].toLocaleString()}</td>
-							<td>(+{status.new_deaths.toLocaleString()})</td>
-							<td>{status[hospitalField].toLocaleString()}</td>
-							<td>
-								({status.new_hospital >= 0 && '+'}
-								{status.new_hospital.toLocaleString()})
-							</td>
-							<td>{status[icuField].toLocaleString()}</td>
-							<td>
-								({status.new_icu >= 0 && '+'}
-								{status.new_icu.toLocaleString()})
-							</td>
-							<td>{status['Resolved'].toLocaleString()}</td>
-							<td>
-								({status.new_resolved >= 0 && '+'}
-								{status.new_resolved.toLocaleString()})
-							</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-			<button
-				type="button"
-				onClick={showMore}
-				style={{ backgroundColor: 'white', color: 'black', border: '2px solid #008CBA', marginRight: '5px' }}
-			>
-				+
-			</button>
-			<button
-				type="button"
-				onClick={showLess}
-				style={{ backgroundColor: 'white', color: 'black', border: '2px solid #008CBA' }}
-			>
-				-
-			</button>
-		</div>
+		<DataTable data={dataSource} columns={columns} />
 	);
 };
 
