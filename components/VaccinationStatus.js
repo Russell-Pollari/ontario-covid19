07068e296044036b856f79b28c52dd19f2a43869
Russell Pollari <pollarir@gmail.com>
2021-03-14 15:48:14 -0400

    Vaccine status (#87)
    * Show vaccine status and other prettifications
    
    * Fixup
    
    Co-authored-by: russell-pollari <pollarir@mgail.com>

--- components/VaccinationStatus.js
+++ components/VaccinationStatus.js
@@ -4,12 +4,14 @@ const VaccinationStatus = ({ data = [] }) => {
 	if (!data.length) {
 		return null;
 	}
-	const population = 14570000;
+
+	const population = 14733119;
+
 	const lastUpdate = data[data.length - 1].total_individuals_fully_vaccinated;
 	return (
 		<div className="mv16">
 			<Typography variant="h6">
-				**{Math.round((lastUpdate / population) * 100)}% of the population Ontario is fully vaccinated.**
+				**~{Math.round((lastUpdate / population) * 100 * 100) / 100}% of the population Ontario is fully vaccinated**
 			</Typography>
 		</div>
 	);
