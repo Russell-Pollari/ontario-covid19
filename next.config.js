81eacc7d925c792897527779730f5ecb984e25f5
russell-pollari <pollarir@mgail.com>
2021-04-05 08:47:38 -0400

    Add base path to next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const basePath = '/ontario-covid19';
 const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
+	basePath: isDev ? '' : basePath,
 	assetPrefix: isDev ? '' : basePath,
 	publicRuntimeConfig: {
 		basePath: isDev ? '' : basePath,
