d230657e71f0d223990b54f1cff3c413a2d3b3e1
Dave Hillis <dwhillis@gmail.com>
2021-04-29 10:15:39 -0400

    Fix linting issue

--- src/data/getVaccineData.js
+++ src/data/getVaccineData.js
@@ -7,7 +7,7 @@ const ensureNumber = (value) => {
     if (typeof value === 'number') {
         return value;
     }
-    return Number((value|| '0').replace(/,/g, ''));
+    return Number((value || '0').replace(/,/g, ''));
 };
 
 const getVaccineData = () =>
