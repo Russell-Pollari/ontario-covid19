c17a3b3e6ece32a1aa630c0e45f2b5cb3859f2e3
aboodmufti <mufti14@gmail.com>
2021-05-19 21:40:35 +0300

    use updated eligible population instead of only adults

--- src/components/VaccinationStatus.js
+++ src/components/VaccinationStatus.js
@@ -5,41 +5,47 @@ const VaccinationStatus = ({ data = [] }) => {
 	if (!data.length) {
 		return null;
 	}
-
-	const population = 11602097;
+	// Population estimates on July 1st 2020 according to Statistics Canada
+	const populationBetween12and18 = 961342
+	const adultPopulation = 11971129
+	const eligiblePopulation = populationBetween12and18 + adultPopulation;
 
 	const lastUpdate = data[data.length - 1];
 	const totalFullyVaccinated = lastUpdate.total_individuals_fully_vaccinated;
 	const totalsWithOneDose = lastUpdate.total_doses_administered - (2 * totalFullyVaccinated);
-	const timeToFullVaccination = ((population - totalFullyVaccinated) * 2 - totalsWithOneDose) / lastUpdate.new_vaccines_rolling_average;
-	const timeToOneDose = ((population - totalFullyVaccinated) - totalsWithOneDose) / lastUpdate.new_vaccines_rolling_average;
+	const timeToFullVaccination = ((eligiblePopulation - totalFullyVaccinated) * 2 - totalsWithOneDose) / lastUpdate.new_vaccines_rolling_average;
+	const timeToOneDose = ((eligiblePopulation - totalFullyVaccinated) - totalsWithOneDose) / lastUpdate.new_vaccines_rolling_average;
 
 	return (
 		<ContentContainer title="Vaccinations at a glance">
 			<ul>
 				<li>
 					<strong>
-						{Math.round(((totalsWithOneDose + totalFullyVaccinated) / population) * 100 * 100) / 100}%
+						{Math.round(((totalsWithOneDose + totalFullyVaccinated) / eligiblePopulation) * 100 * 100) / 100}%
 					</strong>
 					{' '}
-					of the <em>adult</em> population of Ontario has had at least one shot.*
+					of the <em>eligible</em> population of Ontario has had at least one shot.*
 					</li>
 				<li>
 					<strong>
-						{Math.round((totalFullyVaccinated / population) * 100 * 100) / 100}%
+						{Math.round((totalFullyVaccinated / eligiblePopulation) * 100 * 100) / 100}%
 					</strong>
 					{' '}
-					of the <em>adult</em> population of Ontario is fully vaccinated.*
+					of the <em>eligible</em> population of Ontario is fully vaccinated.*
 				</li>
 				<li>
 					At the current average pace of <strong>{lastUpdate.new_vaccines_rolling_average.toLocaleString()}</strong> shots per day.
-					We can deliver a single dose to every adult in <strong>{Math.round(timeToOneDose)} days.</strong> And
+					We can deliver a single dose to every eligible person in <strong>{Math.round(timeToOneDose)} days.</strong> And
 					reach full vaccination in <strong>{Math.round(timeToFullVaccination)} days.*</strong>
 				</li>
 			</ul>
 			<div className="mt16 f12">
+				*Eligible population: Everyone 12 and older based on estimates from Statistics Canada on July 1st 2020.
+				<br/>
 				*These numbers are all back-of-the-envolope estimates. Don't take them as fact.
 			</div>
+			
+			
 		</ContentContainer>
 	);
 };
